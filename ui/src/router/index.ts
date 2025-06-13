f59be23dd92bd74d953403ee31ca6c0a46c23add
Mick Thaweevit <thaweevit.la@sansiri.com>
2025-06-13 17:27:40 +0700

    Fix dynamic import error in router configuration

--- ui/src/router/index.ts
+++ ui/src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import SettingsView from '../views/SettingsView.vue'
+import LoginView from '../views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,12 +14,12 @@ const router = createRouter({
     {
       path: '/settings',
       name: 'settings',
-      component: () => import('../views/SettingsView.vue')
+      component: SettingsView
     },
     {
       path: '/login',
       name: 'login',
-      component: () => import('../views/LoginView.vue')
+      component: LoginView
     }
   ]
 })
